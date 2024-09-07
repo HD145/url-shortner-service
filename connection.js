dffbbd80f0f24e5a7616f7ccc7d4615027767630
Harshit Dwivedi <hdwivedi145@gmail.com>
2024-09-07 23:12:39 +0530

    modifications in connection service

--- connection.js
+++ connection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const connectToDB = ()=>{
-    return mongoose.connect(`mongodb+srv://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@cluster0.uk3x6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`);
+    return mongoose.connect(process.env.MONGO_URI);
 }
 
 module.exports = {
